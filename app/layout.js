8ea86c52ee35386e5af4379617f03f06e851a742
KarRakib <rakibkazi777@gmail.com>
2023-09-27 17:34:25 +0600

    second

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import Navbar from './components/Navbar/Navbar'
+import Provider from './components/Provider'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -13,8 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
-        {children}</body>
+       <Provider>
+       <Navbar/>
+        {children}
+       </Provider>
+        </body>
     </html>
   )
 }
